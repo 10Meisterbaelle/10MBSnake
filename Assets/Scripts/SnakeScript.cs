02eccb2b36e7c1a31e6b39231d582c6b10e00c32
felix <neumaif02@gmail.com>
2024-01-30 10:27:20 +0100

    Added indices (IDs) and extremely basic check system

--- Assets/Scripts/SnakeScript.cs
+++ Assets/Scripts/SnakeScript.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEditor;
 using UnityEngine;
 
@@ -10,10 +11,9 @@ public class SnakeScript : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        GameObject snakeBlock1 = SpawnSquare(0, 0);
-        
-        GameObject snakeBlock2 = SpawnSquare(-0.25f, 0);
-        GameObject snakeBlock3 = SpawnSquare(-0.5f, 0);
+        GameObject snakeBlock1 = SpawnSquare(0, 0, 1);
+        GameObject snakeBlock2 = SpawnSquare(-0.25f, 0, 2);
+        GameObject snakeBlock3 = SpawnSquare(-0.5f, 0, 3);
     }
 
     // Update is called once per frame
@@ -22,9 +22,10 @@ public class SnakeScript : MonoBehaviour
         
     }
 
-    GameObject SpawnSquare(float x, float y)
+    GameObject SpawnSquare(float x, float y, int index)
     {
-        GameObject spawnedSquare = Instantiate(square, new Vector3(x, y, 0), Quaternion.identity);
+        GameObject spawnedSquare = Instantiate(square, new Vector3(x, y, 0), Quaternion.identity, GetComponent<Transform>());
+        spawnedSquare.GetComponent<SquareMetadata>().index = index;
         return spawnedSquare;
     }
 }
--- Assets/Scripts/SquareScript.cs
+++ Assets/Scripts/SquareScript.cs
@@ -6,16 +6,25 @@ using UnityEngine;
 public class SquareScript : MonoBehaviour
 {
     public Transform snake;
-    
+    private SquareMetadata _squareMetadata;
+
     // Start is called before the first frame update
     void Start()
     {
+        _squareMetadata = GetComponent<SquareMetadata>();
         snake = GameObject.FindGameObjectWithTag("Snake").GetComponent<Transform>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (_squareMetadata.index == 1)
+        {
+            transform.position = snake.position;
+        }
+        else
+        {
+            // TODO: Follow the other blocks
+        }
     }
 }
