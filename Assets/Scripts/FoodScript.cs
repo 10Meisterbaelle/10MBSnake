8bd2b629be6ad0f05946117e1c4ee2de6ac362d2
felix <neumaif02@gmail.com>
2024-02-01 12:14:26 +0100

    Added score counter

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -1,3 +1,4 @@
+using TMPro;
 using UnityEngine;
 
 public class FoodScript : MonoBehaviour
@@ -5,12 +6,14 @@ public class FoodScript : MonoBehaviour
     public GameObject snake;
 
     public LogicScript logic;
+    public TMP_Text scoreCounter;
 
     // Start is called before the first frame update
     private void Start()
     {
         snake = GameObject.FindGameObjectWithTag("Snake");
         logic = GameObject.FindGameObjectWithTag("Logic").GetComponent<LogicScript>();
+        scoreCounter = GameObject.FindGameObjectWithTag("ScoreCounter").GetComponent<TMP_Text>();
     }
 
     // Update is called once per frame
@@ -25,6 +28,7 @@ public class FoodScript : MonoBehaviour
             Destroy(gameObject);
             GameObject.FindGameObjectWithTag("FoodSpawner").GetComponent<FoodSpawnScript>().SpawnFood();
             logic.squares.Add(snake.GetComponent<SnakeScript>().SpawnSquare(69, 420));
+            scoreCounter.text = (logic.squares.Count - 3).ToString();
         }
     }
 }
