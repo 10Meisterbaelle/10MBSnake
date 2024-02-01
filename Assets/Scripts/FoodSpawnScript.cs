562dfdc7bbce7032fd621a30f9c3a6526cd988c2
felix <neumaif02@gmail.com>
2024-02-01 14:48:38 +0100

    Cleanup, Reformatting and removed a comment

--- Assets/Scripts/FoodSpawnScript.cs
+++ Assets/Scripts/FoodSpawnScript.cs
@@ -9,16 +9,16 @@ public class FoodSpawnScript : MonoBehaviour
     // Start is called before the first frame update
     private void Start()
     {
-        Instantiate(food, new Vector3(Random.Range(0, 15), Random.Range(0, 14), 0), Quaternion.identity); // Instantiates the food prefab at a random position
+        Instantiate(food, new Vector3(Random.Range(0, 15), Random.Range(0, 14), 0),
+            Quaternion.identity); // Instantiates the food prefab at a random position
     }
-
-    // Update is called once per frame
 
     public void SpawnFood()
     {
         var foodPosition = new Vector3(Random.Range(0, 15), Random.Range(0, 14), 0);
         foreach (var square in logic.squares)
-            if (square.transform.position == foodPosition) // This checks whether the food is spawning inside the snake and cancels the function
+            if (square.transform.position ==
+                foodPosition) // This checks whether the food is spawning inside the snake and cancels the function
             {
                 SpawnFood();
                 return;
