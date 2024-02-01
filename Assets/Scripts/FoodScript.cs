e77324b4ddaa030a992f0395dca91ee80a251273
felix <neumaif02@gmail.com>
2024-02-01 10:43:23 +0100

    Added basic food

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -2,13 +2,12 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FoodSpawnScript : MonoBehaviour
+public class FoodScript : MonoBehaviour
 {
     // Start is called before the first frame update
-    public GameObject food;
     void Start()
     {
-        Instantiate(food, new Vector3(Random.Range(0, 15), Random.Range(0, 14), 0), Quaternion.identity);
+        
     }
 
     // Update is called once per frame
