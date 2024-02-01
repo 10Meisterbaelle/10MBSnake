834803d35a4a6cf516e54e743eed0571e6038dc7
felix <neumaif02@gmail.com>
2024-02-01 14:43:49 +0100

    Removed useless comment

--- Assets/Scripts/FoodScript.cs
+++ Assets/Scripts/FoodScript.cs
@@ -16,8 +16,6 @@ public class FoodScript : MonoBehaviour
         scoreCounter = GameObject.FindGameObjectWithTag("ScoreCounter").GetComponent<TMP_Text>();
     }
 
-    // Update is called once per frame
-
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.layer == 6) // if the food is touched by the snake
